339287229852d2c7f9922c1cbbfe59c4462d548f
Kacper Mianowany <kacper.mianowany@wp.pl>
2026-04-17 16:56:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -71,3 +71,6 @@ if __name__ == "__main__":
     db.create_all()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
